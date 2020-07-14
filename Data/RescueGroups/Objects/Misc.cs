cd3aca5d49e99e49de766143e2a5c0c98128e35d
Cain Watson <cainwatson00@gmail.com>
2020-07-14 18:00:56 -0500

    feat: add animal search model

---
+++ Data/RescueGroups/Objects/Misc.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace PetAdoptionsServer.Data.RescueGroups.Object.Misc
+{
+    public class Image
+    {
+        public string type { get; set; }
+        public string fileSize { get; set; }
+        public string resolutionX { get; set; }
+        public string resolutionY { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Picture
+    {
+        public string mediaID { get; set; }
+        public string mediaOrder { get; set; }
+        public DateTime lastUpdated { get; set; }
+        public string fileSize { get; set; }
+        public string resolutionX { get; set; }
+        public string resolutionY { get; set; }
+        public string fileNameFullsize { get; set; }
+        public string fileNameThumbnail { get; set; }
+        public string urlSecureFullsize { get; set; }
+        public string urlSecureThumbnail { get; set; }
+        public string urlInsecureFullsize { get; set; }
+        public string urlInsecureThumbnail { get; set; }
+        public Image original { get; set; }
+        public Image large { get; set; }
+        public Image small { get; set; }
+    }
+}
---
+++ Data/RescueGroups/Requests/Animals.cs
@@ -0,0 +1,39 @@
+
+namespace PetAdoptionsServer.Data.RescueGroups.Requests.Animals
+{
+    public class PublicSearch
+    {
+        public class Filter {
+            public string fieldName { get; set; }
+            public string operation { get; set; }
+            public string criteria { get; set; }
+        }
+
+        public class SearchOptions {
+            public int resultStart { get; set; }
+            public int resultLimit { get; set; }
+            public string resultSort { get; set; }
+            public string resultOrder { get; set; }
+            public string calcFoundRows { get; set; }
+            public Filter[] filters { get; set; }
+            public string[] fields { get; set; }
+        }
+
+        public string apikey { get; set; }
+        public string objectType
+        {
+            get
+            {
+                return "animals";
+            }
+        }
+        public string objectAction
+        {
+            get
+            {
+                return "publicSearch";
+            }
+        }
+        public SearchOptions search { get; set; }
+    }
+}
